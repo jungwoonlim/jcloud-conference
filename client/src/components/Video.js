d14b9da74379b357acaca9ff01d570e8c01cc3a9
jungwoonlim <wypo23@naver.com>
2021-02-25 02:05:33 +0900

    CSS 업데이트

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,16 +1,35 @@
 import React from "react";
+import styled from "styled-components";
+
+const LocalVideo = styled.video`
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  width: 300px;
+  height: auto;
+`;
+
+const RemoteVideo = styled.video`
+  width: 80%;
+  height: auto;
+`;
 
 function Video() {
   return (
     <div className="video_container" id="videos">
-      <video
+      <LocalVideo
         id="localVideo"
         autoPlay
         playsInline
         controls={false}
         muted={true}
-      ></video>
-      <video id="remoteVideo" autoPlay playsInline controls={false}></video>
+      ></LocalVideo>
+      <RemoteVideo
+        id="remoteVideo"
+        autoPlay
+        playsInline
+        controls={false}
+      ></RemoteVideo>
     </div>
   );
 }
